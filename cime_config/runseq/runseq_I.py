100c34d124b1c7c4ad97324e0baa827bad5c3a69
Mariana Vertenstein <mvertens@ucar.edu>
2019-12-16 14:25:03 -0700

    new I compset run sequence

--- cime_config/runseq/runseq_I.py
+++ cime_config/runseq/runseq_I.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os, shutil, sys
+from CIME.utils import expect
 
 _CIMEROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..","..","..","..")
 sys.path.append(os.path.join(_CIMEROOT, "scripts", "Tools"))
@@ -22,129 +23,106 @@ def runseq(case, coupling_times):
     atm_cpl_dt = coupling_times["atm_cpl_dt"]
     glc_cpl_dt = coupling_times["glc_cpl_dt"]
 
-    if (comp_glc == 'cism'):
+    glc_couplint_time = glc_cpl_dt
+
+    print "DEBUG: i am here0"
+
+    # NOTE: in the mediator the glc_avg_period will be set as an alarm
+    # on the mediator clock - when this alarm rings - the averaging will be done AND an
+    # attribute will be set on the glc export state from the mediator
+    # saying that the data coming to glc is valid
+
+    if (comp_rof == 'srof'):
+        med_to_rof = False
+        rof_to_med = False
+    elif (comp_rof == 'drof'):
+        med_to_rof = False
+        rof_to_med = True
+    else:
+        med_to_rof = True
+        rof_to_med = True
+
+    print "DEBUG: comp_glc = ",comp_glc
+    cism_evolve = False
+    if (comp_glc == 'sglc'):
+        glc_to_med = False
+        med_to_glc = False
+        print "DEBUG: i am here1"
+    elif (comp_glc == 'cism'):
+        print "DEBUG: i am here2"
         cism_evolve = case.get_value("CISM_EVOLVE")
+        glc_to_med = True
+        if cism_evolve:
+            med_to_glc = True
+        else:
+            med_to_glc = False
+    else:
+        expect(False, "Invalid comp_glc %s " %compglc)
+        
 
-    if (comp_rof == 'srof' and comp_glc == "sglc"):
-
-        outfile.write ("runSeq::                                \n" )
-        outfile.write ("@" + str(atm_cpl_dt) + "                \n" )
-        outfile.write ("  MED med_phases_prep_lnd               \n" )
-        outfile.write ("  MED -> LND :remapMethod=redist        \n" )
-        outfile.write ("  LND                                   \n" )
-        outfile.write ("  LND -> MED :remapMethod=redist        \n" )
-        outfile.write ("  MED med_fraction_set                  \n" )
-        outfile.write ("  ATM                                   \n" )
-        outfile.write ("  ATM -> MED :remapMethod=redist        \n" )
-        outfile.write ("  MED med_phases_history_write          \n" )
-        outfile.write ("  MED med_phases_profile                \n" )
-        outfile.write ("  MED med_phases_restart_write          \n" )
-        outfile.write ("@                                       \n" )
-        outfile.write ("::                                      \n" )
-
-    elif ((comp_rof == 'mosart' or comp_rof == 'rtm') and comp_glc == "sglc"):
-
-        outfile.write ("runSeq::                                  \n" )
-        outfile.write ("@" + str(rof_cpl_dt) + "                  \n" )
-        outfile.write ("  MED med_phases_prep_rof_avg             \n" )
-        outfile.write ("  MED -> ROF :remapMethod=redist          \n" )
-        outfile.write ("  ROF                                     \n" )
-        if atm_cpl_dt < rof_cpl_dt:
-            outfile.write ("@" + str(atm_cpl_dt) + "              \n" )
-        outfile.write ("  MED med_phases_prep_lnd                 \n" )
-        outfile.write ("  MED -> LND :remapMethod=redist          \n" )
-        outfile.write ("  LND                                     \n" )
-        outfile.write ("  LND -> MED :remapMethod=redist          \n" )
-        outfile.write ("  MED med_phases_prep_rof_accum           \n" )
-        outfile.write ("  ATM                                     \n" )
-        outfile.write ("  ATM -> MED :remapMethod=redist          \n" )
-        outfile.write ("  MED med_phases_profile                  \n" )
-        if atm_cpl_dt < rof_cpl_dt:
-            outfile.write ("@                                     \n" )
-        outfile.write ("  ROF -> MED :remapMethod=redist          \n" )
-        outfile.write ("  MED med_phases_history_write            \n" )
-        outfile.write ("  MED med_phases_restart_write            \n" )
-        outfile.write ("@                                         \n" )
-        outfile.write ("::                                        \n" )
-
-    elif ((comp_rof == 'mosart' or comp_rof == 'rtm') and comp_glc == "cism" and cism_evolve == False):
-
-        # set glc coupling time  
-        # the default value is glc_cpl_dt
+    # If CISM is not evolving only get data back from cism at the initial time
+    # However will still need to call the exchange at the end if the stop_option
+    # is nsteps or days - or otherwise just every ndays
+    # Note that nsteps is the minimum component coupling time
+    if (comp_glc == 'cism'):
         glc_coupling_time = glc_cpl_dt
-        if (cism_evolve == False):
+        if not cism_evolve:
             stop_option = case.get_value('STOP_OPTION')
             stop_n = case.get_value('STOP_N')
             if stop_option == 'nsteps':
                 glc_coupling_time = stop_n * atm_cpl_dt
             elif stop_option == 'ndays':
                 glc_coupling_time = stop_n * 86400
-            elif stop_option == 'nmonths' or stop_option == 'nyears':
-                # just couple daily
+            else:
                 glc_coupling_time = 86400
 
-        outfile.write ("runSeq::                                  \n" )
-        outfile.write ("@" + str(glc_coupling_time) + "           \n" )
-        outfile.write ("@" + str(rof_cpl_dt) + "                  \n" )
-        outfile.write ("  MED med_phases_prep_rof_avg             \n" )
-        outfile.write ("  MED -> ROF :remapMethod=redist          \n" )
-        outfile.write ("  ROF                                     \n" )
-        if atm_cpl_dt < rof_cpl_dt:
-            outfile.write ("@" + str(atm_cpl_dt) + "              \n" )
-        outfile.write ("  MED med_phases_prep_lnd                 \n" )
-        outfile.write ("  MED -> LND :remapMethod=redist          \n" )
-        outfile.write ("  LND                                     \n" )
-        outfile.write ("  LND -> MED :remapMethod=redist          \n" )
-        outfile.write ("  MED med_phases_prep_rof_accum           \n" )
-        outfile.write ("  MED med_phases_prep_glc_accum           \n" )
-        outfile.write ("  ATM                                     \n" )
-        outfile.write ("  ATM -> MED :remapMethod=redist          \n" )
-        if atm_cpl_dt < rof_cpl_dt:
-            outfile.write ("@                                     \n" )
-        outfile.write ("  ROF -> MED :remapMethod=redist          \n" )
-        outfile.write ("  MED med_phases_history_write            \n" )
-        outfile.write ("  MED med_phases_restart_write            \n" )
-        outfile.write ("  MED med_phases_profile                  \n" )
-        outfile.write ("@                                         \n" )
-        outfile.write ("  GLC cism_invalid_inputs                 \n" )
-        outfile.write ("  GLC -> MED :remapMethod=redist          \n" )
-        outfile.write ("@                                         \n" )
-        outfile.write ("::                                        \n" )
+    outfile.write ("runSeq::                                  \n" )
 
-    elif ((comp_rof == 'mosart' or comp_rof == 'rtm') and comp_glc == "cism" and cism_evolve == True):
+    if glc_to_med or med_to_glc:
+        outfile.write ("@" + str(glc_coupling_time) + "       \n" )
+    if med_to_glc:
+        outfile.write ("  MED med_phases_prep_glc_accum       \n" )
+        outfile.write ("  MED med_phases_prep_glc_avg         \n" )
+        outfile.write ("  MED -> GLC :remapMethod=redist      \n" )
+        outfile.write ("  GLC                                 \n" )
+    if glc_to_med:
+        outfile.write ("  GLC -> MED :remapMethod=redist      \n" )
 
-        outfile.write ("runSeq::                                  \n" )
-        outfile.write ("@" + str(glc_cpl_dt) + "                  \n" )
-        outfile.write ("  MED med_phases_prep_glc_avg             \n" )
-        outfile.write ("  MED -> GLC :remapMethod=redist          \n" )
-        if (cism_evolve == False): 
-            outfile.write ("  GLC cism_invalid_inputs             \n" )
-        else:
-            outfile.write ("  GLC cism_valid_inputs               \n" )
-        outfile.write ("@" + str(rof_cpl_dt) + "                  \n" )
-        outfile.write ("  MED med_phases_prep_rof_avg             \n" )
-        outfile.write ("  MED -> ROF :remapMethod=redist          \n" )
-        outfile.write ("  ROF                                     \n" )
+    if med_to_rof:
+        outfile.write ("@" + str(rof_cpl_dt) + "              \n" )
+        outfile.write ("  MED med_phases_prep_rof_avg         \n" )
+        outfile.write ("  MED -> ROF :remapMethod=redist      \n" )
+        outfile.write ("  ROF                                 \n" )
         if atm_cpl_dt < rof_cpl_dt:
-            outfile.write ("@" + str(atm_cpl_dt) + "              \n" )
-        outfile.write ("  MED med_phases_prep_lnd                 \n" )
-        outfile.write ("  MED -> LND :remapMethod=redist          \n" )
-        outfile.write ("  LND                                     \n" )
-        outfile.write ("  LND -> MED :remapMethod=redist          \n" )
-        outfile.write ("  MED med_phases_prep_rof_accum           \n" )
-        outfile.write ("  MED med_phases_prep_glc_accum           \n" )
-        outfile.write ("  ATM                                     \n" )
-        outfile.write ("  ATM -> MED :remapMethod=redist          \n" )
+            outfile.write ("@" + str(atm_cpl_dt) + "          \n" )
+    else:
+        outfile.write ("@" + str(atm_cpl_dt) + "              \n" )
+
+    outfile.write ("  MED med_phases_prep_lnd                 \n" )
+    outfile.write ("  MED -> LND :remapMethod=redist          \n" )
+    outfile.write ("  LND                                     \n" )
+    outfile.write ("  LND -> MED :remapMethod=redist          \n" )
+    outfile.write ("  MED med_phases_prep_rof_accum           \n" )
+    outfile.write ("  ATM                                     \n" )
+    outfile.write ("  ATM -> MED :remapMethod=redist          \n" )
+    outfile.write ("  MED med_phases_profile                  \n" )
+
+    if rof_to_med:
         if atm_cpl_dt < rof_cpl_dt:
-            outfile.write ("@                                     \n" )
-        outfile.write ("  ROF -> MED :remapMethod=redist          \n" )
-        outfile.write ("@                                         \n" )
-        outfile.write ("  GLC -> MED :remapMethod=redist          \n" )
-        outfile.write ("  MED med_phases_history_write            \n" )
-        outfile.write ("  MED med_phases_restart_write            \n" )
-        outfile.write ("  MED med_phases_profile                  \n" )
-        outfile.write ("@                                         \n" )
-        outfile.write ("::                                        \n" )
+            outfile.write ("@                                 \n" )
+        outfile.write ("  ROF -> MED :remapMethod=redist      \n" )
+
+    outfile.write ("  MED med_phases_history_write            \n" )
+    outfile.write ("  MED med_phases_restart_write            \n" )
+    outfile.write ("  MED med_phases_profile                  \n" )
+
+    if med_to_rof:
+        outfile.write ("@                                     \n" )
+        
+    if glc_to_med or med_to_glc:
+        outfile.write ("@                                     \n" )
+
+    outfile.write ("::                                        \n" )
 
     outfile.close()
     shutil.copy(os.path.join(caseroot, "CaseDocs", "nuopc.runseq"), rundir)
