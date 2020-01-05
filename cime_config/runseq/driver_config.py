4249a28ce674189fc089b7cb7974ecb51e5677fb
Mariana Vertenstein <mvertens@ucar.edu>
2020-01-04 17:28:28 -0700

    more generalization of run sequence generation

--- cime_config/runseq/driver_config.py
+++ cime_config/runseq/driver_config.py
@@ -24,24 +24,22 @@ class DriverConfig(dict):
         comp_atm  = case.get_value("COMP_ATM")
 
         run_atm = True
-        atm_to_med = True
         med_to_atm = True
         if (comp_atm == 'satm'):
             run_atm = False
-            atm_to_med = False
             med_to_atm = False
         elif (comp_atm == 'datm'):
             # TODO: check of data model prognostic flag is on - this is a new xml variable
             # If the prognostic flag is on, then should set med_to_atm to True
             if_prognostic = False
             med_to_atm = if_prognostic
-        else:
-            if (case.get_value('COMP_OCN') == 'socn'):
-                # This is true for adiabatic componsets
-                atm_to_med = False
-                med_to_atm = False
 
-        return (run_atm, atm_to_med, med_to_atm, coupling_times["atm_cpl_dt"])
+        # TODO: need to put in special logical for adiabatic mode - where the atmosphere does
+        # does not really send anything to the mediator
+        # This will be the case if (case.get_value('COMP_OCN') == 'socn'): 
+        # In this case - a special run sequence should be written
+
+        return (run_atm, med_to_atm, coupling_times["atm_cpl_dt"])
 
     ###############################################
     def __compute_glc(self, case, coupling_times):
@@ -57,14 +55,11 @@ class DriverConfig(dict):
 
         run_glc = True
         med_to_glc = True
-        glc_to_med = True
         if (comp_glc == 'sglc'):
             run_glc = False
-            glc_to_med = False
             med_to_glc = False
         elif (comp_glc == 'cism'):
             if not case.get_value("CISM_EVOLVE"):
-                run_glc = False
                 med_to_glc = False
 
         # If CISM is not evolving only get data back from cism at the initial time
@@ -82,10 +77,12 @@ class DriverConfig(dict):
                     glc_coupling_time = stop_n * 86400
                 else:
                     glc_coupling_time = 86400
+        elif (comp_glc == 'xglc'):
+            glc_coupling_time = coupling_times["glc_cpl_dt"]
         else:
             glc_coupling_time = 0
 
-        return (run_glc, glc_to_med, med_to_glc, glc_coupling_time)
+        return (run_glc, med_to_glc, glc_coupling_time)
 
     ###############################################
     def __compute_ice(self, case, coupling_times):
@@ -94,14 +91,12 @@ class DriverConfig(dict):
         comp_ice  = case.get_value("COMP_ICE")
 
         run_ice = True
-        ice_to_med = True
         med_to_ice = True # this is the case for dice
         if (comp_ice == 'sice'):
             run_ice = False
-            ice_to_med = False
             med_to_ice = False
         
-        return (run_ice, ice_to_med, med_to_ice, coupling_times["ice_cpl_dt"])
+        return (run_ice, med_to_ice, coupling_times["ice_cpl_dt"])
 
     ###############################################
     def __compute_lnd(self, case, coupling_times):
@@ -110,11 +105,9 @@ class DriverConfig(dict):
         comp_lnd  = case.get_value("COMP_LND")
 
         run_lnd = True
-        lnd_to_med = True
         med_to_lnd = True
         if (comp_lnd == 'slnd'):
             run_lnd = False
-            lnd_to_med = False
             med_to_lnd = False
         elif (comp_lnd == 'dlnd'):
             # TODO: check of data model prognostic flag is on - this is a new xml variable
@@ -122,7 +115,7 @@ class DriverConfig(dict):
             if_prognostic = False
             med_to_lnd = if_prognostic
 
-        return (run_lnd, lnd_to_med, med_to_lnd, coupling_times["lnd_cpl_dt"])
+        return (run_lnd, med_to_lnd, coupling_times["lnd_cpl_dt"])
 
     ###############################################
     def __compute_ocn(self, case, coupling_times):
@@ -131,11 +124,9 @@ class DriverConfig(dict):
         comp_ocn = case.get_value("COMP_OCN")
 
         run_ocn = True
-        ocn_to_med = True
         med_to_ocn = True
         if (comp_ocn == 'socn'):
             run_ocn = False
-            ocn_to_med = False
             med_to_ocn = False
         elif (comp_ocn == 'docn'):
             # TODO: check of data model prognostic flag is on - this is a new xml variable
@@ -143,7 +134,7 @@ class DriverConfig(dict):
             docn_mode = case.get_value("DOCN_MODE")
             med_to_ocn = ('som' in docn_mode or 'interannual' in docn_mode)
 
-        return (run_ocn, ocn_to_med, med_to_ocn, coupling_times["ocn_cpl_dt"])
+        return (run_ocn, med_to_ocn, coupling_times["ocn_cpl_dt"])
 
     ###############################################
     def __compute_rof(self, case, coupling_times):
@@ -152,11 +143,9 @@ class DriverConfig(dict):
         comp_rof  = case.get_value("COMP_ROF")
 
         run_rof = True
-        rof_to_med = True 
         med_to_rof = True
         if (comp_rof == 'srof'):
             run_rof = False
-            rof_to_med = False
             med_to_rof = False
         elif (comp_rof == 'drof'):
             # TODO: check of data model prognostic flag is on - this is a new xml variable
@@ -168,9 +157,9 @@ class DriverConfig(dict):
             # otherwise if clm is not active then assume data lnd for now
             if (case.get_value("COMP_LND") == 'clm'):
                 if (case.get_value("CLM_ACCELERATED_SPINUP") == 'on'):
-                    rof_to_med = False
+                    run_rof = False
 
-        return (run_rof, rof_to_med, med_to_rof, coupling_times["rof_cpl_dt"])
+        return (run_rof, med_to_rof, coupling_times["rof_cpl_dt"])
 
     ###############################################
     def __compute_wav(self, case, coupling_times):
@@ -179,11 +168,9 @@ class DriverConfig(dict):
         comp_wav  = case.get_value("COMP_WAV")
 
         run_wav = True
-        wav_to_med = True
         med_to_wav = True
         if (comp_wav == 'swav'):
             run_wav = False
-            wav_to_med = False
             med_to_wav = False
         elif (comp_wav == 'dwav'):
             # TODO: check of data model prognostic flag is on - this is a new xml variable
@@ -191,5 +178,5 @@ class DriverConfig(dict):
             if_prognostic = False
             med_to_wav = if_prognostic
 
-        return (run_wav, wav_to_med, med_to_wav, coupling_times["wav_cpl_dt"])
+        return (run_wav, med_to_wav, coupling_times["wav_cpl_dt"])
 
--- cime_config/runseq/runseq_general.py
+++ cime_config/runseq/runseq_general.py
@@ -19,111 +19,123 @@ def gen_runseq(case, coupling_times):
     caseroot       = case.get_value("CASEROOT")
     cpl_seq_option = case.get_value('CPL_SEQ_OPTION')
 
+    # It is assumed that if a component will be run it will send information to the mediator
+    # so the flags run_xxx and xxx_to_med are redundant
+
     driver_config = DriverConfig(case, coupling_times)
-    run_atm, atm_to_med, med_to_atm, atm_cpl_time = driver_config['atm']
-    run_ice, ice_to_med, med_to_ice, ice_cpl_time = driver_config['ice']
-    run_glc, glc_to_med, med_to_glc, glc_cpl_time = driver_config['glc']
-    run_lnd, lnd_to_med, med_to_lnd, lnd_cpl_time = driver_config['lnd']
-    run_ocn, ocn_to_med, med_to_ocn, ocn_cpl_time = driver_config['ocn']  
-    run_rof, rof_to_med, med_to_rof, rof_cpl_time = driver_config['rof']
-    run_wav, wav_to_med, med_to_wav, wav_cpl_time = driver_config['wav']
+    run_atm, med_to_atm, atm_cpl_time = driver_config['atm']
+    run_ice, med_to_ice, ice_cpl_time = driver_config['ice']
+    run_glc, med_to_glc, glc_cpl_time = driver_config['glc']
+    run_lnd, med_to_lnd, lnd_cpl_time = driver_config['lnd']
+    run_ocn, med_to_ocn, ocn_cpl_time = driver_config['ocn']  
+    run_rof, med_to_rof, rof_cpl_time = driver_config['rof']
+    run_wav, med_to_wav, wav_cpl_time = driver_config['wav']
 
     # Note: assume that atm_cpl_dt, lnd_cpl_dt, ice_cpl_dt and wav_cpl_dt are the same
+
+    if lnd_cpl_time != atm_cpl_time:
+        expect(False, "assume that lnd_cpl_time is equal to atm_cpl_time")
+    if ice_cpl_time != atm_cpl_time:
+        expect(False, "assume that ice_cpl_time is equal to atm_cpl_time")
+    if wav_cpl_time != atm_cpl_time:
+        expect(False, "assume that wav_cpl_time is equal to atm_cpl_time")
+    if rof_cpl_time < ocn_cpl_time:
+        expect(False, "assume that rof_cpl_time is always greater than or equal to ocn_cpl_time")
+
+    glc_outer_loop = run_glc
+    rof_outer_loop = run_rof and rof_cpl_time > atm_cpl_time
+    ocn_outer_loop = run_ocn and ocn_cpl_time > atm_cpl_time  
+
+    inner_loop = ((atm_cpl_time < ocn_cpl_time) or
+                  (atm_cpl_time < rof_cpl_time) or 
+                  (run_glc and atm_cpl_time < glc_cpl_time) or
+                  atm_cpl_time == ocn_cpl_time)
 
     with RunSeq(os.path.join(caseroot, "CaseDocs", "nuopc.runseq")) as runseq:
 
         #------------------
-        runseq.enter_time_loop(glc_cpl_time, newtime=(glc_to_med or med_to_glc), active=med_to_glc)
+        runseq.enter_time_loop(glc_cpl_time, newtime=run_glc, active=med_to_glc)
         #------------------
 
         runseq.add_action("MED med_phases_prep_glc_avg"    , med_to_glc)
         runseq.add_action("MED -> GLC :remapMethod=redist" , med_to_glc)
-        runseq.add_action("GLC"                            , run_glc)
-        runseq.add_action("GLC -> MED :remapMethod=redist" , glc_to_med)
+        runseq.add_action("GLC"                            , run_glc and med_to_glc)
+        runseq.add_action("GLC -> MED :remapMethod=redist" , run_glc)
 
         #------------------
-        runseq.enter_time_loop(rof_cpl_time, newtime=rof_to_med)
+        runseq.enter_time_loop(rof_cpl_time, newtime=rof_outer_loop)
         #------------------
 
-        runseq.add_action("MED med_phases_prep_rof_avg"    , med_to_rof)
-        runseq.add_action("MED -> ROF :remapMethod=redist" , med_to_rof)
-        runseq.add_action("ROF"                            , run_rof and atm_cpl_time < rof_cpl_time)
+        runseq.add_action("MED med_phases_prep_rof_avg"    , med_to_rof and rof_outer_loop)
+        runseq.add_action("MED -> ROF :remapMethod=redist" , med_to_rof and rof_outer_loop)
+        runseq.add_action("ROF"                            , rof_outer_loop)
 
         #------------------
-        runseq.enter_time_loop(ocn_cpl_time, newtime=((ocn_cpl_time < rof_cpl_time) or (not rof_to_med)))
+        runseq.enter_time_loop(ocn_cpl_time, newtime=ocn_outer_loop)
         #------------------
+
+        runseq.add_action("MED med_phases_prep_ocn_accum_avg"  , med_to_ocn and ocn_outer_loop)
+        runseq.add_action("MED -> OCN :remapMethod=redist"     , med_to_ocn and ocn_outer_loop)
         
-        if (cpl_seq_option == 'RASM'):
-            runseq.add_action("MED med_phases_prep_ocn_accum_avg"  , med_to_ocn and atm_cpl_time < ocn_cpl_time)
-            runseq.add_action("MED -> OCN :remapMethod=redist"     , med_to_ocn and atm_cpl_time < ocn_cpl_time)
-            runseq.add_action("OCN"                                , run_ocn    and atm_cpl_time < ocn_cpl_time)
-            runseq.add_action("OCN -> MED :remapMethod=redist"     , ocn_to_med and atm_cpl_time < ocn_cpl_time)
-        else:
-            runseq.add_action("MED med_phases_prep_ocn_accum_avg"  , med_to_ocn and atm_cpl_time == ocn_cpl_time)
-            runseq.add_action("MED -> OCN :remapMethod=redist"     , med_to_ocn and atm_cpl_time == ocn_cpl_time)
-
         #------------------
-        runseq.enter_time_loop(atm_cpl_time, newtime=((atm_cpl_time < ocn_cpl_time)))
+        runseq.enter_time_loop(atm_cpl_time, newtime=inner_loop)
         #------------------
 
         if (cpl_seq_option == 'RASM'):
-            runseq.add_action("MED med_phases_prep_ocn_accum_avg"  , med_to_ocn and atm_cpl_time == ocn_cpl_time)
-            runseq.add_action("MED -> OCN :remapMethod=redist"     , med_to_ocn and atm_cpl_time == ocn_cpl_time)
-            runseq.add_action("OCN"                                , run_ocn)
-            runseq.add_action("OCN -> MED :remapMethod=redist"     , ocn_to_med)
             runseq.add_action("MED med_phases_prep_ocn_map"        , med_to_ocn)
             runseq.add_action("MED med_phases_aofluxes_run"        , run_ocn and run_atm and (med_to_ocn or med_to_atm))
             runseq.add_action("MED med_phases_prep_ocn_merge"      , med_to_ocn)
             runseq.add_action("MED med_phases_prep_ocn_accum_fast" , med_to_ocn)
             runseq.add_action("MED med_phases_ocnalb_run"          , run_ocn and run_atm and (med_to_ocn or med_to_atm))
-
         runseq.add_action("MED med_phases_prep_lnd"                , med_to_lnd)
         runseq.add_action("MED -> LND :remapMethod=redist"         , med_to_lnd)
-
-        runseq.add_action("MED med_phases_prep_ice"                , ice_to_med)
-        runseq.add_action("MED -> ICE :remapMethod=redist"         , ice_to_med)
-
+        runseq.add_action("MED med_phases_prep_ice"                , med_to_ice)
+        runseq.add_action("MED -> ICE :remapMethod=redist"         , med_to_ice)
+        runseq.add_action("MED med_phases_prep_wav"                , med_to_wav)
+        runseq.add_action("MED -> WAV :remapMethod=redist"         , med_to_wav)
+        runseq.add_action("MED med_phases_prep_rof_avg"            , med_to_rof and not rof_outer_loop)
+        runseq.add_action("MED -> ROF :remapMethod=redist"         , med_to_rof and not rof_outer_loop)
+        runseq.add_action("MED med_phases_prep_ocn_accum_avg"      , med_to_ocn and not ocn_outer_loop)
+        runseq.add_action("MED -> OCN :remapMethod=redist"         , med_to_ocn and not ocn_outer_loop)
         runseq.add_action("ICE"                                    , run_ice)
-        runseq.add_action("ROF"                                    , run_rof and atm_cpl_time == rof_cpl_time)
         runseq.add_action("LND"                                    , run_lnd)
-
-        runseq.add_action("WAV"                                    , run_wav )
-        runseq.add_action("WAV -> MED :remapMethod=redist"         , wav_to_med)
-
-        if (cpl_seq_option == 'TIGHT'):
-            runseq.add_action("OCN"                                , run_ocn)
-            runseq.add_action("OCN -> MED :remapMethod=redist"     , ocn_to_med)
-
-        runseq.add_action("ICE -> MED :remapMethod=redist"         , ice_to_med)
-        runseq.add_action("MED med_fraction_set"                   , ice_to_med)
-
+        runseq.add_action("ROF"                                    , run_rof and not rof_outer_loop)
+        runseq.add_action("WAV"                                    , run_wav)
+        runseq.add_action("OCN"                                    , run_ocn and not ocn_outer_loop)
+        runseq.add_action("OCN -> MED :remapMethod=redist"         , run_ocn and not ocn_outer_loop)
         if (cpl_seq_option == 'TIGHT'):
             runseq.add_action("MED med_phases_prep_ocn_map"        , med_to_ocn)
-            runseq.add_action("MED med_phases_aofluxes_run"        , ocn_to_med and atm_to_med)
+            runseq.add_action("MED med_phases_aofluxes_run"        , run_ocn and run_atm)
             runseq.add_action("MED med_phases_prep_ocn_merge"      , med_to_ocn)
             runseq.add_action("MED med_phases_prep_ocn_accum_fast" , med_to_ocn)
-            runseq.add_action("MED med_phases_ocnalb_run"          , ocn_to_med and atm_to_med)
-
-        runseq.add_action("LND -> MED :remapMethod=redist"         , lnd_to_med)
+            runseq.add_action("MED med_phases_ocnalb_run"          , run_ocn and run_atm)
+        runseq.add_action("LND -> MED :remapMethod=redist"         , run_lnd)
+        runseq.add_action("ICE -> MED :remapMethod=redist"         , run_ice)
+        runseq.add_action("MED med_fraction_set"                   , run_ice)
         runseq.add_action("MED med_phases_prep_rof_accum"          , med_to_rof)
         runseq.add_action("MED med_phases_prep_glc_accum"          , med_to_glc)
-
         runseq.add_action("MED med_phases_prep_atm"                , med_to_atm)
         runseq.add_action("MED -> ATM :remapMethod=redist"         , med_to_atm)
         runseq.add_action("ATM"                                    , run_atm)
-        runseq.add_action("ATM -> MED :remapMethod=redist"         , atm_to_med)
-
-        #runseq.add_action("MED med_phases_history_write"          , rof_to_med)
-
-        #------------------
-        runseq.leave_time_loop(rof_to_med and (atm_cpl_time < rof_cpl_time))
-        #------------------
-
-        runseq.add_action("ROF -> MED :remapMethod=redist" , rof_to_med)
-        runseq.add_action("MED med_phases_history_write"   , True)
+        runseq.add_action("ATM -> MED :remapMethod=redist"         , run_atm)
+        runseq.add_action("WAV -> MED :remapMethod=redist"         , run_wav)
+        runseq.add_action("ROF -> MED :remapMethod=redist"         , run_rof and not rof_outer_loop)
 
         #------------------
-        runseq.leave_time_loop(rof_to_med and (atm_cpl_time < rof_cpl_time))
+        runseq.leave_time_loop(inner_loop)
+        #------------------
+
+        runseq.add_action("OCN"                                    , run_ocn and ocn_outer_loop)
+        runseq.add_action("OCN -> MED :remapMethod=redist"         , run_ocn and ocn_outer_loop)
+
+        #------------------
+        runseq.leave_time_loop(ocn_outer_loop)
+        #------------------
+
+        runseq.add_action("ROF -> MED :remapMethod=redist"         , run_rof and rof_outer_loop)
+
+        #------------------
+        runseq.leave_time_loop(rof_outer_loop)
         #------------------
 
     shutil.copy(os.path.join(caseroot, "CaseDocs", "nuopc.runseq"), rundir)
