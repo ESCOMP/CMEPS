90a7516a76baa0affc0e377bf2db929c4f3a5d62
Mariana Vertenstein <mvertens@ucar.edu>
2020-01-04 19:56:00 -0700

    fixed bug

--- cime_config/runseq/runseq_TG.py
+++ cime_config/runseq/runseq_TG.py
@@ -19,8 +19,8 @@ def gen_runseq(case, coupling_times):
     caseroot       = case.get_value("CASEROOT")
 
     driver_config = DriverConfig(case, coupling_times)
-    run_glc, glc_to_med, med_to_glc, glc_cpl_time = driver_config['glc']
-    run_lnd, lnd_to_med, _         , lnd_cpl_time = driver_config['lnd']
+    run_glc, med_to_glc, glc_cpl_time = driver_config['glc']
+    run_lnd,  _        , lnd_cpl_time = driver_config['lnd']
 
     if lnd_cpl_time != glc_cpl_time:
         expect(False,"for TG compset require that lnd_cpl_time equal glc_cpl_time")
@@ -30,13 +30,13 @@ def gen_runseq(case, coupling_times):
         runseq.enter_time_loop(glc_cpl_time, True)
 
         runseq.add_action ("LND"                            , run_lnd)
-        runseq.add_action ("LND -> MED :remapMethod=redist" , lnd_to_med)
-        runseq.add_action ("MED med_fraction_set"           , lnd_to_med )
-        runseq.add_action ("MED med_phases_prep_glc_accum"  , med_to_glc )
-        runseq.add_action ("MED med_phases_prep_glc_avg"    , med_to_glc )
-        runseq.add_action ("MED -> GLC :remapMethod=redist" , med_to_glc,)
-        runseq.add_action ("GLC"                            , run_glc )
-        runseq.add_action ("GLC -> MED :remapMethod=redist" , glc_to_med)
+        runseq.add_action ("LND -> MED :remapMethod=redist" , run_lnd)
+        runseq.add_action ("MED med_fraction_set"           , run_lnd)
+        runseq.add_action ("MED med_phases_prep_glc_accum"  , med_to_glc)
+        runseq.add_action ("MED med_phases_prep_glc_avg"    , med_to_glc)
+        runseq.add_action ("MED -> GLC :remapMethod=redist" , med_to_glc)
+        runseq.add_action ("GLC"                            , run_glc)
+        runseq.add_action ("GLC -> MED :remapMethod=redist" , run_glc)
         runseq.add_action ("MED med_phases_history_write"   , True)
         
         runseq.leave_time_loop(True)
