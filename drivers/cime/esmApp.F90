22a8bd825bfde7e1f6e1f7d8694f0531104bbd93
Jim Edwards <jedwards@ucar.edu>
2021-01-13 09:36:12 -0700

    latest changes

--- drivers/cime/esmApp.F90
+++ drivers/cime/esmApp.F90
@@ -169,11 +169,13 @@ program esmApp
   ! Call Finalize for the ensemble driver
   ! Destroy the ensemble driver
   !-----------------------------------------------------------------------------
+
   call ESMF_GridCompFinalize(ensemble_driver_comp, userRc=urc, rc=rc)
   if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
        line=__LINE__, &
        file=__FILE__)) &
        call ESMF_Finalize(endflag=ESMF_END_ABORT)
+  print *,__FILE__,__LINE__
   if (ESMF_LogFoundError(rcToCheck=urc, msg=ESMF_LOGERR_PASSTHRU, &
        line=__LINE__, &
        file=__FILE__)) &
