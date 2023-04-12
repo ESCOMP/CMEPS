4cf3e05eb4505c6944b137948f9a93f17e96bc7a
Paul Hall <45795415+phall-brown@users.noreply.github.com>
2023-04-12 14:31:14 -0400

    Added Fwxx_taux and Fwxx_tauy, based on Foxx_taux and Foxx_tauy

--- mediator/esmFldsExchange_cesm_mod.F90
+++ mediator/esmFldsExchange_cesm_mod.F90
@@ -2963,6 +2963,41 @@ contains
           call addmrg_to(compwav, 'Sa_tbot', mrg_from=compatm, mrg_fld='Sa_tbot', mrg_type='copy')
        end if
     end if
+!PSH begin
+    ! ---------------------------------------------------------------------
+    ! to wav: zonal and meridional wind stress
+    ! ---------------------------------------------------------------------
+    if (phase == 'advertise') then
+       call addfld_to(compwav , 'Fwxx_taux')
+       call addfld_from(compice , 'Fioi_taux')
+       call addfld_aoflux('Faox_taux')
+    else
+       if ( fldchk(is_local%wrap%FBexp(compwav), 'Fwxx_taux', rc=rc)) then
+          if (fldchk(is_local%wrap%FBimp(compice,compice), 'Fioi_taux', rc=rc)) then
+             call addmap_from(compice, 'Fioi_taux', compocn, mapfcopy, 'unset', 'unset')
+             call addmrg_to(compwav, 'Fwxx_taux', &
+                  mrg_from=compice, mrg_fld='Fioi_taux', mrg_type='merge', mrg_fracname='ifrac')
+          end if
+          call addmrg_to(compwav, 'Fwxx_taux', &
+               mrg_from=compmed, mrg_fld='Faox_taux', mrg_type='merge', mrg_fracname='ofrac')
+       end if     
+    end if
+    if (phase == 'advertise') then
+       call addfld_to(compwav , 'Fwxx_tauy')
+       call addfld_from(compice , 'Fioi_tauy')
+       call addfld_aoflux('Faox_tauy')
+    else  
+       if ( fldchk(is_local%wrap%FBexp(compwav), 'Foxx_tauy', rc=rc)) then
+          if (fldchk(is_local%wrap%FBimp(compice,compice), 'Fioi_tauy', rc=rc)) then
+             call addmap_from(compice, 'Fioi_tauy', compocn, mapfcopy, 'unset', 'unset')
+             call addmrg_to(compwav, 'Fwxx_tauy', &
+                  mrg_from=compice, mrg_fld='Fioi_tauy', mrg_type='merge', mrg_fracname='ifrac')
+          end if  
+          call addmrg_to(compwav, 'Fwxx_tauy', &
+               mrg_from=compmed, mrg_fld='Faox_tauy', mrg_type='merge', mrg_fracname='ofrac')
+       end if
+    end if
+!PSH end
 
     !=====================================================================
     ! FIELDS TO RIVER (comprof)
