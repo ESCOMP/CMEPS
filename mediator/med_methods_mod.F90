8f59dbaa6bb113141d26c81d23538d8f4779bfae
Jim Edwards <jedwards@ucar.edu>
2023-05-24 10:04:39 -0600

    try building ext with cdeps share

--- mediator/med_methods_mod.F90
+++ mediator/med_methods_mod.F90
@@ -2530,10 +2530,6 @@ contains
     ! ----------------------------------------------
     rc = ESMF_SUCCESS
 
-#ifndef CESMCOUPLED
-    ! For now only CESM uses shr_infnan_isnan - so until other models provide this
-    mediator_checkfornans = .false.
-#endif
     if(.not. mediator_checkfornans) return
     
     call ESMF_FieldBundleGet(FB, fieldCount=fieldCount, rc=rc)
@@ -2572,8 +2568,6 @@ contains
   end subroutine med_methods_FB_check_for_nans
 
   !-----------------------------------------------------------------------------
-#ifdef CESMCOUPLED
-
   subroutine med_methods_check_for_nans_1d(dataptr, nancount)
     use shr_infnan_mod, only: shr_infnan_isnan
     ! input/output variables
@@ -2607,27 +2601,5 @@ contains
        end do
     end do
   end subroutine med_methods_check_for_nans_2d
-
-#else
-
-  ! For now only CESM uses shr_infnan_isnan - so until other models provide this
-  ! nancount will just be set to zero
-
-  subroutine med_methods_check_for_nans_1d(dataptr, nancount)
-    ! input/output variables
-    real(r8) , intent(in)  :: dataptr(:)
-    integer  , intent(out) :: nancount
-
-    nancount = 0
-  end subroutine med_methods_check_for_nans_1d
-
-  subroutine med_methods_check_for_nans_2d(dataptr, nancount)
-    ! input/output variables
-    real(r8) , intent(in)  :: dataptr(:,:)
-    integer  , intent(out) :: nancount
-
-    nancount = 0
-  end subroutine med_methods_check_for_nans_2d
-#endif
 
 end module med_methods_mod
