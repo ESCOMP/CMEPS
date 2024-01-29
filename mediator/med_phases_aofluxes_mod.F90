64e1c276dee0de636b03a9bd1186fb794699b02d
uturuncoglu <turuncu@ucar.edu>
2024-01-28 19:35:04 -0700

    fix Faxa_rainc issue when add_gusts is turned on

--- mediator/med_phases_aofluxes_mod.F90
+++ mediator/med_phases_aofluxes_mod.F90
@@ -1597,6 +1597,10 @@ end subroutine med_aofluxes_map_ogrid2xgrid_input
        if (chkerr(rc,__LINE__,u_FILE_u)) return
        call fldbun_getfldptr(fldbun_a, 'Sa_shum', aoflux_in%shum, xgrid=xgrid, rc=rc)
        if (chkerr(rc,__LINE__,u_FILE_u)) return
+       if (add_gusts) then
+          call fldbun_getfldptr(fldbun_a, 'Faxa_rainc', aoflux_in%rainc, xgrid=xgrid, rc=rc)
+          if (chkerr(rc,__LINE__,u_FILE_u)) return
+       end if
     end if
 
     ! extra fields for ufs.frac.aoflux
