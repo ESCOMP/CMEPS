6d7a65575d9084215c0b2b479bdb1c0aebd8c425
Steve Penny <StevePny@users.noreply.github.com>
2024-06-18 02:34:39 -0600

    Create cmeps.mk
    create template for makefile fragment

---
+++ cmeps.mk
@@ -0,0 +1,15 @@
+#-----------------------------------------------
+# NUOPC/ESMF self-describing build dependency
+# makefile fragment for CMEPS
+#-----------------------------------------------
+
+# component module name
+MED_ESMF_DEP_FRONT := MED
+# component module path
+MED_ESMF_DEP_INCPATH := 
+# component module objects
+MED_ESMF_DEP_CMPL_OBJS := 
+# component object/archive list
+MED_ESMF_DEP_LINK_OBJS := 
+MED_ESMF_DEP_SHRD_PATH := 
+MED_ESMF_DEP_SHRD_LIBS := 
