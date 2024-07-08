0e019ed23f2133ab51b50a889c7952219954a645
StevePny <stevepenny@hotmail.com>
2024-07-08 21:56:22 +0000

    updates to makefile fragment

--- cmeps.mk
+++ cmeps.mk
@@ -4,6 +4,17 @@
 #-----------------------------------------------
 
 # component module name
+#MED_ESMF_DEP_FRONT := MED
+#MED_ESMF_DEP_SHRD_PATH := /opt/PIO/lib /tmp/CMEPS/mediator /tmp/CMEPS/shared /tmp/CMEPS/ufs /opt/libFMS/intel/32bit /opt/nceplibs/lib
+#MED_ESMF_DEP_SHRD_LIBS := pioc piof cmeps shared cmeps_share
+
+# component module name
 MED_ESMF_DEP_FRONT := MED
-MED_ESMF_DEP_SHRD_PATH := 
+# component module path
+MED_ESMF_DEP_INCPATH := /opt/PIO/include
+# component module objects
+MED_ESMF_DEP_CMPL_OBJS := /tmp/CMEPS/mediator/*.o /tmp/CMEPS/shared/*.o /tmp/CMEPS/ufs/*.o
+# component object/archive list
+MED_ESMF_DEP_LINK_OBJS := /tmp/CMEPS/mediator/libcmeps.a /tmp/CMEPS/shared/libshared.a /tmp/CMEPS/ufs/libcmeps_share.a
+MED_ESMF_DEP_SHRD_PATH := /opt/PIO/lib  /opt/libFMS/intel/32bit /opt/nceplibs/lib /tmp/CMEPS/mediator /tmp/CMEPS/shared /tmp/CMEPS/ufs
 MED_ESMF_DEP_SHRD_LIBS := pioc piof cmeps shared cmeps_share
