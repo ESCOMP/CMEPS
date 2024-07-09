2c751cb62d42ca70b96c45a3ab50c1d8c8b8f553
StevePny <stevepenny@hotmail.com>
2024-07-09 06:40:44 +0000

    wildcard expansion on cmeps .o files

--- cmeps.mk
+++ cmeps.mk
@@ -13,7 +13,7 @@ MED_ESMF_DEP_FRONT := MED
 # component module path
 MED_ESMF_DEP_INCPATH := /opt/PIO/include
 # component module objects
-MED_ESMF_DEP_CMPL_OBJS := /tmp/CMEPS/mediator/*.o /tmp/CMEPS/shared/*.o /tmp/CMEPS/ufs/*.o
+MED_ESMF_DEP_CMPL_OBJS := $(/tmp/CMEPS/mediator/*.o) $(/tmp/CMEPS/shared/*.o) $(/tmp/CMEPS/ufs/*.o)
 # component object/archive list
 MED_ESMF_DEP_LINK_OBJS := /tmp/CMEPS/mediator/libcmeps.a /tmp/CMEPS/shared/libshared.a /tmp/CMEPS/ufs/libcmeps_share.a
 MED_ESMF_DEP_SHRD_PATH := /opt/PIO/lib  /opt/libFMS/intel/32bit /opt/nceplibs/lib /tmp/CMEPS/mediator /tmp/CMEPS/shared /tmp/CMEPS/ufs
