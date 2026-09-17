0b39c0a75759d221d694caa76db2af9127db66bf
Bill Sacks <sacks@ucar.edu>
2026-09-17 17:07:27 -0600

    Better wording in comment

--- mediator/med_global_sums_mod.F90
+++ mediator/med_global_sums_mod.F90
@@ -141,7 +141,7 @@ contains
 
     ! local variables
     ! Note that summands_2d is deliberately allocatable, rather than being an automatic array or
-    ! an inline reshape of local_summands: those would give an array temporary, which some
+    ! an inline reshape of local_summands: those would create an array temporary, which some
     ! compilers place on the stack, and local_summands can be large (e.g., of order the number of
     ! local grid cells).
     real(r8), allocatable :: summands_2d(:,:)
